d0dbbb4e4ba6ebdb14f3ed7ee7b1d4a39b771cec
cthierst <chlothier@gmail.com>
2023-02-02 11:54:49 -0500

    cleaned death registry data, added references to bib, updated paper writing and code, updated read me file, re-organized files, created simulation

--- 01-data_cleaning.R
+++ 01-data_cleaning.R
@@ -1,31 +1,50 @@
 
 #### Preamble ####
-# Purpose: Read in data from Open Data Toronto and clean the civic centre data from 2012 downloaded from Open Data Toronto's statistics on death registration. Link: https://open.toronto.ca/dataset/death-registry-statistics/
+# Purpose: Read in data from Open Data Toronto and clean the civic centre data from 2012 downloaded from Open Data Toronto's statistics on death registration.Link: https://open.toronto.ca/dataset/death-registry-statistics/
 # Author: Chloe Thierstein
 # Email: chloe.thierstein@utoronto.ca
 # Date: 23 January 2023
 # Prerequisites: Need to know where to find the City of Toronto death registry data
 
-#### workspace set-up ####
+#| include: false
+
+#### Workspace Set-up ####
+
 #### install.packages(tidyverse) ####
 #### install.packages(janitor) ####
+#### install.packages(ggplot2) ####
 #### install.packages("knitr") ####
+#### install.packages("RColorBrewer") ####
+#### install.packages("kableExtra") ####
 
-install.packages("RColorBrewer")
-library("RColorBrewer")
-
-library(knitr)
 library(tidyverse)
+library(opendatatoronto)
 library(janitor)
+library(ggplot2)
+library(dplyr)
+library(knitr)
+library(RColorBrewer)
+library(kableExtra)
 
 
+#### Read in Data ####
+raw_death_registry_data <-
+  read_csv(
+    file = 
+      "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/cba07a90-984b-42d2-9131-701c8c2a9788/resource/cc418325-b3d0-4afd-ae64-ccb9b71c549c/download/Death%20Registry%20Statistics%20Data.csv",
+    show_col_types = FALSE,
+    skip = 0
+  )
+
+write_csv(
+  x = raw_death_registry_data,
+  file = "death_registry.csv"
+)
 
 
 #### Clean Names ####
 cleaned_death_registry_data <-
   clean_names(raw_death_registry_data)
-
-head(cleaned_death_registry_data)
 
 
 #### Simplifying Date #### 
@@ -33,20 +52,21 @@ cleaned_death_registry_data <-
   cleaned_death_registry_data |>
   separate(
     col = time_period,
-    into = c("year", "month"),
+    into = c("Year", "Month"),
     sep = "-"
   ) |>
-  select(civic_centre, death_licenses, place_of_death, year, month)
+  select(civic_centre, death_licenses, place_of_death, Year, Month)
 
-#### Removing 2021-2022 rows ####
-cleaned_death_registry_data <- cleaned_death_registry_data[-c(746:841), ] 
+#### Removing 2021-2023 data ####
+cleaned_death_registry_data <- cleaned_death_registry_data[-c(1:371,746:847), ] 
+
 
 #### Recoding Months to be more Meaningful ####
 cleaned_death_registry_data <- cleaned_death_registry_data |> 
   mutate( 
-    month = 
+    Month = 
       recode( 
-        month, 
+        Month, 
         "01" = "Jan", 
         "02" = "Feb", 
         "03" = "Mar",
@@ -62,6 +82,7 @@ cleaned_death_registry_data <- cleaned_death_registry_data |>
       ) 
   )
 
+
 #### Recoding Civic Centres to be more Meaningful ####
 cleaned_death_registry_data <- cleaned_death_registry_data |> 
   mutate( 
@@ -74,58 +95,17 @@ cleaned_death_registry_data <- cleaned_death_registry_data |>
       ) 
   )
 
+#### ordering months #####
+cleaned_death_registry_data$Month <- factor(cleaned_death_registry_data$Month, levels=c("Jan", "Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))
+
+
+#### filtering out toronto civic centre from data ####
 cleaned_death_registry_data <- cleaned_death_registry_data |>
   filter(!grepl('TO', civic_centre))
 
-#### Ordering Months #####
-cleaned_death_registry_data$month <- factor(cleaned_death_registry_data$month, levels=c("Jan", "Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))
-
-
-############## Graphing ##################
-
-
-#### Creating Graph ####
-ggplot(data=cleaned_death_registry_data, aes(x = year, y = death_licenses)) +
-  geom_bar(stat = "identity") +
-  theme_minimal() + 
-  labs(
-    x = "Year",
-    y = "Death Licenses"
-  )
-
-
-
-#### Creating Graph ####
-ggplot(data=cleaned_death_registry_data, aes(x = month, y = death_licenses, fill = month)) +
-  scale_fill_brewer(palette="Set3") + theme_minimal() +
-  geom_bar(stat = "identity") +
-  labs(
-    x = "Month",
-    y = "Death Licenses"
-  ) 
-
-
-
-#### Creating Graph ####
-ggplot(data=cleaned_death_registry_data, aes(x = place_of_death, y = death_licenses, fill = place_of_death)) +
-  geom_bar(stat = "identity") +
-  theme_minimal() +
-  labs(
-    x = "Place of Death",
-    y = "Death Licenses"
-  ) +
-  scale_fill_brewer(palette="Set3")
-
-
-
-#### Creating Graph ####
-ggplot(data=cleaned_death_registry_data, aes(x = civic_centre, y = death_licenses, fill = year)) +
-  geom_bar(stat = "identity") +
-  theme_minimal() + 
-  labs(
-    x = "Civic Centre",
-    y = "Death Licenses"
-  )
-
-
+#### creating a CSV file for cleaned data ####
+write_csv(
+  x = cleaned_death_registry_data,
+  file = "cleaned_death_registry.csv"
+)
 
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# starter_folder
+# Overview
 
-This repo....
+Death registry statistics have been updated monthly since 2011 are available on the Open Data Toronto Portal. I use this data to analyze the
 
-It is organised as follows...
+# Note on File Structure
